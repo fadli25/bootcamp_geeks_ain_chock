5ae2400a150098bf8223d42ec063caa2838e8e6c
fadli25 <fadlizakaria222@gmail.com>
2025-08-12 14:41:27 +0100

    init

--- week_1/day_1/daily_challengs/daily_challenge.py
+++ week_1/day_1/daily_challengs/daily_challenge.py
@@ -1,4 +1,22 @@
 # ------ Challenge 1
 
+number = int(input("Enter a number: "))
+length = int(input("Enter the lenght: "))
+multiple_number = 0
+multiple_list = []
+
+for i in range(1 , length + 1):
+    multiple_number += number
+    multiple_list.append(multiple_number)
+
+print(multiple_list) 
+
 # ------ Challenge 2
 
+word = input("Enter a word: ")
+
+word = list(word)
+word = set(word)
+word = "".join(word)
+
+print(word)
